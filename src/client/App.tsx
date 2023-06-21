4ccc65081140172471c15f00b96b92d4775bb7c3
Guilherme Cardim <guilherme.rccarvalho@al.infnet.edu.br>
2023-06-21 11:17:50 -0300

    Update

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import { Home } from "./routes/Home";
+//import { Home } from "./routes/Home";
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<h1>olá</h1>} />
       </Routes>
     </BrowserRouter>
   );
